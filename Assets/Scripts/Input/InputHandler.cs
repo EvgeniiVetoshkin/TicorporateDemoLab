3808a5227a6c2ac4f377506b91f60056a25e809f
EvgeniiVetoshkin <AA2892@student.jamk.fi>
2022-09-06 17:01:50 +0300

    MovementTest

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -2,10 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DefaultExecutionOrder(-239)]
 public class InputHandler : MonoBehaviour
 {
+    public static InputHandler instance;
+
     public Vector2 InputVector { get; private set; }
     public Vector2 MousePosition { get; private set; }
+
+
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
 
     private void Update()
     {
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private Animator playerAnimator;
+    private InputHandler input;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        input = GetComponent<InputHandler>();
+    }
+    private void Update()
+    {
+        playerAnimator.SetFloat("Vertical", input.InputVector.y);
+        playerAnimator.SetFloat("Vertical", input.InputVector.x);
     }
 }
