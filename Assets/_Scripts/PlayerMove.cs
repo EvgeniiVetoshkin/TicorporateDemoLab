ff5218ce830ac238552872ae88d6194426aabed6
EvgeniiVetoshkin <AA2892@student.jamk.fi>
2022-09-07 19:42:11 +0300

    nothing really

--- Assets/_Scripts/PlayerMove.cs
+++ Assets/_Scripts/PlayerMove.cs
@@ -9,10 +9,15 @@ public class PlayerMove : MonoBehaviour
     private Animator playerAnimator;
     private float acceleration = 5f, maxVelocity = 1f;
     private Vector2 velocity = Vector2.zero;
+    private Vector3 cameraRotation;
 
 
     private void Update()
     {
+
+        cameraRotation = Vector3.ProjectOnPlane(Camera.main.transform.rotation.eulerAngles.normalized, Vector3.up);
+        
+
         velocity = Vector2.MoveTowards(velocity, InputHandler.instance.InputVector * maxVelocity, acceleration * Time.deltaTime);
             
         playerAnimator.SetFloat("Vertical", velocity.y);
